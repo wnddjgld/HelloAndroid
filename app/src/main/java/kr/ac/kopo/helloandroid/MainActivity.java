e290afabaf115d319db161a77450eddd47c42cff
wnddjgld <wnddjgld@gmail.com>
2025-03-17 16:29:14 +0900

    EditText에 입력 받아서 결과 표 처리 결과 표시

--- app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package kr.ac.kopo.helloandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        TextView text1 = findViewById(R.id.text111);
+        EditText edit1 = findViewById(R.id.editName);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "버튼을 클릭하셨습니다.", Toast.LENGTH_LONG);
+                toast.show();
+                String name = edit1.getText().toString();
+                text1.setText(name + "입니다.");
+            }
         });
     }
+
+
 }
